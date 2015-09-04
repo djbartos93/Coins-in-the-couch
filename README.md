7319970492eaa97cf9342873fd412f97c419a708
djbartos93 <djbartos93@gmail.com>
2015-09-04 00:26:39 -0400

    Basic functions test
    gets data from couch potato api and puts it in a file.

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple movie list for couchpotato.
+
+Lists out movies in a better format than the default movie list. allows for better sorting and searching.
---
+++ couchview.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'uri'
+require 'yaml'
+require 'json'
+
+def list
+  puts "getting movies"
+
+  uri = URI.parse('http://172.16.0.15:5050/api/0c7afb2c910d49b2aa98b5f762e62b98/movie.list')
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+  #print response.body
+  output = File.open('./db/temp_db.yml', 'w')
+  output.puts YAML.dump(JSON.parse response.body)
+  debug = JSON.parse response.body
+  print debug['movies'][0]['title']
+end
+
+
+list
