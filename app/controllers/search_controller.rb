31d6d704c9afff979ead2649823215ce1478445d
Eric Bartos <djbartos93@gmail.com>
2015-09-12 20:53:50 -0400

    search kinda working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,15 @@
 class SearchController < ApplicationController
-def query
-    @records = Movie.search "*"
-    @records.each do |record|
-      puts record
-    end
+  def query
+    @records = Movie.search(params[:search], index_name: [Movie.searchkick_index.name]).results
+
     if @records.length == 1
       redirect_to @records.first
       return
     end
-      respond_to do |format|
-        format.html
-        format.json {render json: @records}
+
+    respond_to do |format|
+      format.html # query.html.erb
+      format.json { render json: @records }
     end
   end
 end
