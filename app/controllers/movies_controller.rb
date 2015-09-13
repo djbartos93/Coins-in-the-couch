80080163bdc6cfd16b035ca3c8ddd0b26ed1f09f
Eric Bartos <djbartos93@gmail.com>
2015-09-13 02:37:32 -0400

    fix quality sync

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -69,17 +69,16 @@ class MoviesController < ApplicationController
     response = http.request(request)
     output = JSON.parse response.body
     output['movies'].compact.each do |movie_info|
+      print movie_info.to_yaml
       print movie_info['info']['original_title'].to_yaml
       print movie_info['info']['directors'].to_yaml
       print movie_info['info']['genres'].to_yaml
       print movie_info['info']['year'].to_yaml
-      print movie_info['releases'][0]['quality']
-      print movie_info.to_yaml
       Movie.find_or_create_by(:imdb_id => movie_info['info']['imdb']).update(:title => movie_info['info']['original_title'],
       :director => movie_info['info']['directors'],
       :genre => movie_info['info']['genres'],
       :year => movie_info['info']['year'],
-      :quality => movie_info['releases'][0]['quality'],
+      :quality => movie_info['releases'].empty? ? "None" : movie_info['releases'][0]['quality'],
       :imdb_id => movie_info['info']['imdb'])
     end
   end
