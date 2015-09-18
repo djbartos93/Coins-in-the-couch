06b44464a456f82f90b30797653bf9f3f5179c8a
Eric Bartos <djbartos93@gmail.com>
2015-09-17 21:49:29 -0400

    broke as fucccc

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,25 @@
 class SettingsController < ApplicationController
-  def edit
-      @setting = Setting.find(params[:key])
-    end
+  before_action :set_settings, only: [:edit, :update, :show]
+  def index
+    @settings = Setting.all
+  end
+
+  def new
+    @settings = Setting.new
+  end
+
+
+  def update
+    @settings.update(params)
+  end
+
+  private
+
+     def params
+        params.require(:key).permit(:value, :etc)
+     end
+     def set_settings
+       @settings = Setting.find params[:key]
+     end
 
 end
